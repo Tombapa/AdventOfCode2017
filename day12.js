32576b5eeb2142f02ef01f10cea47debda45522d
Tombapa <tommi.paavilainen@gmail.com>
2017-12-14 22:37:07 +0200

    Init d12

---
+++ day12.js
@@ -0,0 +1,96 @@
+var fs = require('fs');
+linenumber = 0;
+var programnumber = 0;
+var output = "<pre>";
+
+
+// ** Run ** //
+
+
+// ** Functions ** //
+
+function day12() {
+    
+}
+
+
+// ** Read and write ** //
+
+function readInputFile(inputFile, processLine) {
+    console.log("readInputFile(inputFile, processLine): start.");
+    output += "readInputFile(inputFile, processLine): start.\\n\\\n"
+    var remaining = '';
+    
+    try {
+        inputFile.on('data', function(data) {
+            console.log("readInputFile(inputFile, processLine).inputFile.on('data', function(data)).");
+            output += "readInputFile(inputFile, processLine).inputFile.on('data', function(data)).\\n\\\n"
+
+            remaining += data;
+            var index = remaining.indexOf('\n');
+            var last  = 0;
+            while (index > -1 && !needToStop) {
+                var line = remaining.substring(last, index - 1);
+                last = index + 1;
+                
+                processLine(line);
+                
+        
+                index = remaining.indexOf('\n', last);
+            }
+            remaining = remaining.substring(last);
+        });
+    }
+    catch (e) {
+        console.log("\n** readInputFile(inputFile, processLine): Epic fail at inputFile.on('data', function(data)). **\n" + e);
+        output += "\\n\\\n** readInputFile(inputFile, processLine): Epic fail at inputFile.on('data', function(data)). **\\n\\\n" + e + "\\n\\\n";
+    }
+    
+    if (!needToStop) {
+        try {
+            inputFile.on('end', function() {
+                if (remaining.length > 0) {
+                    // console.log("readInputFile(inputFile, processLine).inputFile.on('end', function()): remaining=='" + remaining + "'.");
+                    // output += "readInputFile(inputFile, processLine).inputFile.on('end', function()): remaining=='" + remaining + "'.\\n\\\n";
+                    processLine(remaining);
+                }
+                console.log("readInputFile(inputFile, processLine).inputFile.on('end', function()): moving to afterReading().");
+                output += "readInputFile(inputFile, processLine).inputFile.on('end', function()): moving to afterReading().\\n\\\n";
+                afterReading();
+            });
+        }
+        catch (e) {
+            console.log("\n** readInputFile(inputFile, processLine): Epic fail at inputFile.on('end', function()). **\n" + e);
+            output += "\\n\\\n** readInputFile(inputFile, processLine): Epic fail at inputFile.on('end', function()). **\\n\\\n" + e + "\\n\\\n";
+        }
+
+        // console.log("readInputFile(inputFile, processLine): the very end, there's nothing to see here.");
+    }
+}
+
+
+function processLine(data) {
+    linenumber++;
+    // console.log("processLine(" + data + "): linenumber==" + linenumber);
+    try {
+        var currentReport = data.split(" ");
+        for (i = 0; i < currentReport.length; i++) {
+            currentReport[i] = currentReport[i].replace(/\(|\)|,/g,'');
+            if (i == 1) {
+                currentReport[i] = parseInt(currentReport[i]);
+            }
+        }
+        reports[linenumber] = currentReport.slice();
+        if(linenumber < reportLogCutoff) {
+            console.log("processLine(): reports[" + linenumber + "] == " + reports[linenumber]);
+        }
+    }
+    catch (e) {
+        console.log(
+            "** processLine(data): Epic fail at linenumber " + linenumber 
+            // + " with data=='" + data + "'"
+            + ". **\n" + e);
+        output += "** processLine(data): Epic fail at linenumber " + linenumber + " with data==\'" + data + "\'. **\\n\\\n" + e + "\\n\\\n";
+        needToStop = true;
+    }
+}
