588d20bc8a93ecb879dbe589b764618654b2c4a9
Tombapa <tommi.paavilainen@gmail.com>
2017-12-17 00:48:41 +0200

    D12, nollaan jo liitetyn ohjelman tunnistus ei toimi.

--- day12.js
+++ day12.js
@@ -1,16 +1,57 @@
 var fs = require('fs');
-linenumber = 0;
+var linenumber = 0;
 var programnumber = 0;
 var output = "<pre>";
+var inputLines = [];
+var inputLogCutoff = 3;
+var needToStop = false;
+var loggedSteps = [0, 3, 9];
+var programs = [];
+var groupContaining0 = [];
 
 
 // ** Run ** //
 
+day12();
 
 // ** Functions ** //
 
+
+function Program(index) {
+    if (loggedSteps.includes(index)) {
+        console.log("\nnew Program(" + index + ")");
+        output += "\\n\\\nnew Program(" + index + ")\\n\\\n";
+        // console.dir(myVectors);
+    }
+    this.index = index;
+    this.connectedPrograms = [];
+    this.distanceFrom0 = null;
+    this.addConnectedProgram = function(program) {
+        this.connectedPrograms[this.connectedPrograms.length] = program;
+    }
+    this.toString = function() {
+        var output = "Program " + this.index;
+        if (this.connectedPrograms.length > 0) {
+            output += " <-> ";
+            for (var i = 0; i < this.connectedPrograms.length; i++) {
+                output += this.connectedPrograms[i].index;
+                if (i != (this.connectedPrograms.length - 1)) {
+                    output += ", ";
+                }
+            }
+        }
+        return output;
+    }
+}
+
+
 function day12() {
+    var inputFileName = "attachments\\day12input.txt";
+    var inputFile = fs.createReadStream(inputFileName);
+    console.log("\nday12(): Hello, World! About to read " + inputFileName + ".");
+    output += "\\n\\\nday12(): Hello, World! About to read " + inputFileName.replace("\\", "\\\\") + ".\\n\\\n";
     
+    readInputFile(inputFile, processLine);
 }
 
 
@@ -18,7 +59,7 @@ function day12() {
 
 function readInputFile(inputFile, processLine) {
     console.log("readInputFile(inputFile, processLine): start.");
-    output += "readInputFile(inputFile, processLine): start.\\n\\\n"
+    output += "readInputFile(inputFile, processLine): start.\\n\\\n";
     var remaining = '';
     
     try {
@@ -54,9 +95,10 @@ function readInputFile(inputFile, processLine) {
                     // output += "readInputFile(inputFile, processLine).inputFile.on('end', function()): remaining=='" + remaining + "'.\\n\\\n";
                     processLine(remaining);
                 }
-                console.log("readInputFile(inputFile, processLine).inputFile.on('end', function()): moving to afterReading().");
-                output += "readInputFile(inputFile, processLine).inputFile.on('end', function()): moving to afterReading().\\n\\\n";
-                afterReading();
+                console.log("readInputFile(inputFile, processLine).inputFile.on('end', function()): almost done! Proceeding to the next function.");
+                output += "readInputFile(inputFile, processLine).inputFile.on('end', function()): almost done! Proceeding to the next function.\\n\\\n";
+
+                parsePrograms();
             });
         }
         catch (e) {
@@ -70,20 +112,20 @@ function readInputFile(inputFile, processLine) {
 
 
 function processLine(data) {
-    linenumber++;
     // console.log("processLine(" + data + "): linenumber==" + linenumber);
     try {
-        var currentReport = data.split(" ");
-        for (i = 0; i < currentReport.length; i++) {
-            currentReport[i] = currentReport[i].replace(/\(|\)|,/g,'');
-            if (i == 1) {
-                currentReport[i] = parseInt(currentReport[i]);
+        var currentLine = data.split(" ");
+        for (i = 0; i < currentLine.length; i++) {
+            currentLine[i] = currentLine[i].replace(/\(|\)|,/g,'');
+            if (i != 1) {
+                currentLine[i] = parseInt(currentLine[i]);
             }
         }
-        reports[linenumber] = currentReport.slice();
-        if(linenumber < reportLogCutoff) {
-            console.log("processLine(): reports[" + linenumber + "] == " + reports[linenumber]);
+        inputLines[linenumber] = currentLine.slice();
+        if(linenumber < inputLogCutoff) {
+            console.log("processLine(): inputLines[" + linenumber + "] == " + inputLines[linenumber]);
         }
+        linenumber++;
     }
     catch (e) {
         console.log(
@@ -93,4 +135,107 @@ function processLine(data) {
         output += "** processLine(data): Epic fail at linenumber " + linenumber + " with data==\'" + data + "\'. **\\n\\\n" + e + "\\n\\\n";
         needToStop = true;
     }
+}
+
+
+function parsePrograms() {
+    console.log("parsePrograms(): start.");
+    output += "parsePrograms(): start.\\n\\\n";
+
+    // console.dir(inputLines);
+
+    for (var i = 0; i < inputLines.length; i++) {
+        programs[i] = new Program(i);
+        if (loggedSteps.includes(i)) {
+            console.log(programs[i].toString());
+            output += "\\n\\\n" + programs[i].toString() + ")\\n\\\n";
+        }
+    }
+    // console.dir(programs);
+
+    for (var i = 0; i < programs.length; i++) {
+        // add connected programs
+
+        for (var j = 2; j < inputLines[i].length; j++) {
+            programs[i].addConnectedProgram(programs[inputLines[i][j]]);        
+        }
+
+        if (loggedSteps.includes(i)) {
+            console.log(programs[i].toString());
+            console.dir(programs[i]);
+            output += "\\n\\\n" + programs[i].toString() + ")\\n\\\n";
+        }
+    }   
+
+    console.log("parsePrograms(): almost done! Proceeding to the next function.");
+    output += "parsePrograms(): almost done! Proceeding to the next function.\\n\\\n";
+    getConnectedPrograms();
+}
+
+
+function getConnectedPrograms() {
+    console.log("getConnectedPrograms(): start.");
+    output += "getConnectedPrograms(): start.\\n\\\n";
+
+    groupContaining0[0] = programs[0];
+    programs[0].distanceFrom0 = 0;
+
+    var newLevelFound = true;
+    var currentLevel = 0;
+
+    while (newLevelFound && currentLevel < 2) {
+        console.log("getConnectedPrograms(): currentLevel == " + currentLevel + ".");
+        output += "getConnectedPrograms(): currentLevel == " + currentLevel + ".\\n\\\n";
+        newLevelFound = false;
+
+        for (var i = 0; i < groupContaining0.length; i++) {
+            
+            if (groupContaining0[i].distanceFrom0 == currentLevel) {
+                var currentParent = groupContaining0[i];
+                
+                
+                for (var j = 0; j < currentParent.connectedPrograms.length; j++) {
+                    var currentProrgram = currentParent.connectedPrograms[j];
+                    console.log("getConnectedPrograms(): currentProgram == " + currentProrgram.toString());
+                    if (!isConnectedTo0(currentProrgram)) {
+                        currentProrgram.distanceFrom0 = currentLevel + 1;
+                        groupContaining0[groupContaining0.length] = currentProrgram;
+                        
+                        if (!newLevelFound) {
+                            newLevelFound = true;
+                        }
+                    }
+                }
+            }
+        }
+
+        groupContaining0.sort();
+        console.dir(groupContaining0);
+
+        if (newLevelFound) {
+            console.log("getConnectedPrograms(): a new level has been found! Currently " + groupContaining0.length + " programs connected to 0.");
+            output += "getConnectedPrograms(): a new level has been found!\\n\\\n";
+            currentLevel++;
+        }
+        else {
+            console.log("getConnectedPrograms(): " + currentLevel + " is the last level.");
+            output += "getConnectedPrograms(): " + currentLevel + " is the last level.\\n\\\n";
+        }
+    }
+    
+
+
+}
+
+
+function isConnectedTo0(program) {
+    console.dir(groupContaining0);
+    for (var i = 0; i < groupContaining0.length; i++) {
+        if (program.index = groupContaining0[i].index) {
+            console.log("isConnectedTo0(): program " + program.index + " is connected to 0.");
+            return true;
+        }
+    }
+    console.log("isConnectedTo0(): program " + program.index + " is not connected to 0.");
+    return false;
 }
